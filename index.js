88435a7f971d890b44e087f6567ca57ae36cd7ac
Pedro Duarte <contact@peduarte.com>
2015-07-26 12:02:21 +0100

    backwards slash

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/api/google-form', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-	res.send('Nothing to see here ¯/\_(ツ)_/¯');
+	res.send('Nothing to see here ¯\\_(ツ)_/¯');
 });
 
 
